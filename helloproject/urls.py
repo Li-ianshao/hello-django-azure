1ab6eba45ab109c881708beea2f4e8ccbdf35adb
Shao Li Yan <poi7415778@gmail.com>
2025-05-22 18:54:31 -0500

    Add clock app with digital clock page

--- helloproject/urls.py
+++ helloproject/urls.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('clock.urls')),  # 指到 clock app
+]
+
 
 def home(request):
     return HttpResponse("Hello, world! This is a Django app deployed on Azure.")
 
-urlpatterns = [
-    path('', home),
-]
