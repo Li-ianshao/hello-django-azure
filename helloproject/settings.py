f52e048cab51ea3ffcc91fd9bd36c27f5f27e589
Shao Li Yan <poi7415778@gmail.com>
2025-05-23 17:29:08 -0500

    Fix static files path and collectstatic

--- helloproject/settings.py
+++ helloproject/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,6 +31,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 ROOT_URLCONF = "helloproject.urls"
 
