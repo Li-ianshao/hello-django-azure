87509b968cd42141f6ea35a39d54ea5c4a5c113e
Shao Li Yan <poi7415778@gmail.com>
2025-05-28 10:09:34 -0500

    修改城測試用首頁

--- helloproject/urls.py
+++ helloproject/urls.py
@@ -4,7 +4,8 @@ from django.views.generic.base import RedirectView
 from django.urls import reverse_lazy
 
 urlpatterns = [
-    path('', RedirectView.as_view(url=reverse_lazy('login'))),  # 預設導向登入頁
+    path('', include('stocks.urls'))
+    #path('', RedirectView.as_view(url=reverse_lazy('login'))),  # 預設導向登入頁
     path('stocks/', include('stocks.urls')),
     path('accounts/', include('django.contrib.auth.urls')),     # 提供 /login /logout 等功能
     path('admin/', admin.site.urls),
